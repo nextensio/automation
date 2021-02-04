2c77f1caa694f2e799cdd1c0ec9d291f8f38e64e
Gopa Kumar <gopakumarce@gmail.com>
2021-02-04 11:03:46 -0500

    per cluster extfile

--- testbed/kind/create.sh
+++ testbed/kind/create.sh
@@ -85,10 +85,13 @@ function create_cluster {
     kind load docker-image registry.gitlab.com/nextensio/cluster/minion:latest --name $cluster
     kind load docker-image registry.gitlab.com/nextensio/clustermgr/mel-deploy:latest --name $cluster
 
+    EXTFILE="$tmpdir/$cluster-extfile.conf"
+    echo "subjectAltName = DNS:gateway.$cluster.nextensio.net" > "${EXTFILE}"
     # Create ssl keys/certificates for agents/connectors to establish secure websocket
     openssl req -out $tmpdir/$cluster-gw.csr -newkey rsa:2048 -nodes -keyout $tmpdir/$cluster-gw.key \
         -subj "/CN=gateway.$cluster.nextensio.net/O=Nextensio Gateway $cluster"
-    openssl x509 -req -days 365 -CA $tmpdir/rootca.crt -CAkey $tmpdir/rootca.key -set_serial 0 -in $tmpdir/$cluster-gw.csr -out $tmpdir/$cluster-gw.crt
+    openssl x509 -req -days 365 -CA $tmpdir/rootca.crt -CAkey $tmpdir/rootca.key -set_serial 0 \
+        -in $tmpdir/$cluster-gw.csr -out $tmpdir/$cluster-gw.crt -extfile "${EXTFILE}"
 }
 
 function bootstrap_cluster {

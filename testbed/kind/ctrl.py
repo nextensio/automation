b4fae40004fecf6345dc5af08d5ad14a245a2d67
busyb95120 <busyb95120@gmail.com>
2021-02-11 17:34:46 -0800

    Simplifiy python based create_xxx api's for populating mongoDB. Make them deal with json blobs so that the schemas are defined by the api users. Add apis for host and extended user attributes.
    @gopa

--- testbed/kind/ctrl.py
+++ testbed/kind/ctrl.py
@@ -16,25 +16,30 @@ if __name__ == '__main__':
         print('Controller not up, waiting ...')
         time.sleep(5)
 
-    ok = create_gateway(url, "gateway.testa.nextensio.net")
+    gw1json = {"name":"gateway.testa.nextensio.net"}
+    gw2json = {"name":"gateway.testc.nextensio.net"}
+    
+    ok = create_gateway(url, gw1json)
     while not ok:
         print('Gateway creation failed, retrying ...')
         time.sleep(1)
-        ok = create_gateway(url, "gateway.testa.nextensio.net")
+        ok = create_gateway(url, gw1json)
 
-    ok = create_gateway(url, "gateway.testc.nextensio.net")
+    ok = create_gateway(url, gw2json)
     while not ok:
         print('Gateway creation failed, retrying ...')
         time.sleep(1)
-        ok = create_gateway(url, "gateway.testc.nextensio.net")
+        ok = create_gateway(url, gw2json)
 
-    ok = create_tenant(url, "Test", ["gateway.testa.nextensio.net","gateway.testc.nextensio.net"], 
-                       ["kismis.org"], "registry.gitlab.com/nextensio/cluster/minion:latest", 5)
+    tenantjson = {"name":"Test", "gateways":["gateway.testa.nextensio.net","gateway.testc.nextensio.net"],
+                  "domains": ["kismis.org"], "image":"registry.gitlab.com/nextensio/cluster/minion:latest",
+                  "pods":5, "curid":""}
+    
+    ok = create_tenant(url, tenantjson)
     while not ok:
         print('Tenant creation failed, retrying ...')
         time.sleep(1)
-        ok = create_tenant(url, "Test", ["gateway.testa.nextensio.net","gateway.testc.nextensio.net"], 
-                           ["kismis.org"], "registry.gitlab.com/nextensio/cluster/minion:latest", 5)
+        ok = create_tenant(url, tenantjson)
 
     ok, tenants = get_tenants(url)
     while not ok:
@@ -46,83 +51,110 @@ if __name__ == '__main__':
     # to search for the right tenant name or something inside the returned list of tenants
     tenant = tenants[0]['_id']
 
-    ok = create_user(url, 'test1@nextensio.net', tenant, 'Test User1', 'test1@nextensio.net', ['test1-nextensio-net'], 'gateway.testa.nextensio.net', 1)
+    user1json = {"uid":"test1@nextensio.net", "tenant":tenant, "name":"User1", "email":"test1@nextensio.net",
+                 "services":["test1-nextensio-net"], "gateway":"gateway.testa.nextensio.net", "pod":1}
+    user2json = {"uid":"test2@nextensio.net", "tenant":tenant, "name":"User2", "email":"test2@nextensio.net",
+                 "services":["test2-nextensio-net"], "gateway":"gateway.testa.nextensio.net", "pod":2}
+
+    user1attrjson = {"uid":"test1@nextensio.net", "tenant":tenant, "category":"employee",
+                     "type":"IC", "level":50, "dept":["ABU","BBU"], "team":["engineering"] }
+    user2attrjson = {"uid":"test2@nextensio.net", "tenant":tenant, "category":"employee",
+                     "type":"IC", "level":50, "dept":["ABU","BBU"], "team":["sales"] }
+    
+    bundle1json = {"bid":"default@nextensio.net", "tenant":tenant, "name":"Default Internet",
+                   "services":["default-internet"], "gateway":"gateway.testc.nextensio.net", "pod":3}
+    bundle2json = {"bid":"v1.kismis@nextensio.net", "tenant":tenant, "name":"Kismis ONE",
+                   "services":["v1.kismis.org"], "gateway":"gateway.testc.nextensio.net", "pod":4}
+    bundle3json = {"bid":"v2.kismis@nextensio.net", "tenant":tenant, "name":"Kismis TWO",
+                   "services":["v2.kismis.org"], "gateway":"gateway.testc.nextensio.net", "pod":5}
+
+    bundle1attrjson = {"bid":"default@nextensio.net", "tenant":tenant, "dept":["ABU","BBU"],
+                       "team":["engineering","sales"], "IC":1, "manager":1, "nonemployee":"allow"}
+    bundle2attrjson = {"bid":"v1.kismis@nextensio.net", "tenant":tenant, "dept":["ABU","BBU"],
+                       "team":["engineering","sales"], "IC":1, "manager":1, "nonemployee":"allow"}
+    bundle3attrjson = {"bid":"v2.kismis@nextensio.net", "tenant":tenant, "dept":["ABU","BBU"],
+                       "team":["engineering","sales"], "IC":1, "manager":1, "nonemployee":"allow"}
+    
+    ok = create_user(url, user1json)
     while not ok:
         print('User creation failed, retrying ...')
         time.sleep(1)
-        ok = create_user(url, 'test1@nextensio.net', tenant, 'Test User1', 'test1@nextensio.net', ['test1-nextensio-net'], 'gateway.testa.nextensio.net', 1)
+        ok = create_user(url, user1json)
 
-    ok = create_user_attr(url, 'test1@nextensio.net', tenant, 'employee', 'IC', 50, ['ABU,BBU'], ['engineering','sales'])
+    ok = create_user_attr(url, user1attrjson)
     while not ok:
         print('UserAttr creation failed, retrying ...')
         time.sleep(1)
-        ok = create_user_attr(url, 'test1@nextensio.net', tenant, 'employee', 'IC', 50, ['ABU,BBU'], ['engineering','sales'])
+        ok = create_user_attr(url, user1attrjson)
     
-    ok = create_user(url, 'test2@nextensio.net', tenant, 'Test User2', 'test2@nextensio.net', ['test2-nextensio-net'], 'gateway.testa.nextensio.net', 2)
+    ok = create_user(url, user2json)
     while not ok:
         print('User creation failed, retrying ...')
         time.sleep(1)
-        ok = create_user(url, 'test2@nextensio.net', tenant, 'Test User2', 'test2@nextensio.net', ['test2-nextensio-net'], 'gateway.testa.nextensio.net', 2)
+        ok = create_user(url, user2json)
     
-    ok = create_user_attr(url, 'test2@nextensio.net', tenant, 'employee', 'IC', 50, ['ABU,BBU'], ['engineering','sales'])
+    ok = create_user_attr(url, user2attrjson)
     while not ok:
         print('UserAttr creation failed, retrying ...')
         time.sleep(1)
-        ok = create_user_attr(url, 'test2@nextensio.net', tenant, 'employee', 'IC', 50, ['ABU,BBU'], ['engineering','sales'])
+        ok = create_user_attr(url, user2attrjson)
 
-    ok = create_bundle(url, 'default@nextensio.net', tenant, 'Default Internet Route', ['default-internet'], 'gateway.testc.nextensio.net', 3)
+    ok = create_bundle(url, bundle1json)
     while not ok:
         print('Bundle creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle(url, 'default@nextensio.net', tenant, 'Default Internet Route', ['default-internet'], 'gateway.testc.nextensio.net', 3)
+        ok = create_bundle(url, bundle1json)
 
-    ok = create_bundle_attr(url, 'default@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+    ok = create_bundle_attr(url, bundle1attrjson)
     while not ok:
         print('BundleAttr creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle_attr(url, 'default@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+        ok = create_bundle_attr(url, bundle1attrjson)
 
-    ok = create_bundle(url, 'v1.kismis@nextensio.net', tenant, 'Kismis Version ONE', ['v1.kismis.org'], 'gateway.testc.nextensio.net', 4)
+    ok = create_bundle(url, bundle2json)
     while not ok:
         print('Bundle creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle(url, 'v1.kismis@nextensio.net', tenant, 'Kismis Version ONE', ['v1.kismis.org'], 'gateway.testc.nextensio.net', 4)
+        ok = create_bundle(url, bundle2json)
 
-    ok = create_bundle_attr(url, 'v1.kismis@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+    ok = create_bundle_attr(url, bundle2attrjson)
     while not ok:
         print('BundleAttr creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle_attr(url, 'v1.kismis@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+        ok = create_bundle_attr(url, bundle2attrjson)
 
-    ok = create_bundle(url, 'v2.kismis@nextensio.net', tenant, 'Kismis Version ONE', ['v2.kismis.org'], 'gateway.testc.nextensio.net', 5)
+    ok = create_bundle(url, bundle3json)
     while not ok:
         print('Bundle creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle(url, 'v2.kismis@nextensio.net', tenant, 'Kismis Version ONE', ['v2.kismis.org'], 'gateway.testc.nextensio.net', 5)
+        ok = create_bundle(url, bundle3json)
         
-    ok = create_bundle_attr(url, 'v2.kismis@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+    ok = create_bundle_attr(url, bundle3attrjson)
     while not ok:
         print('BundleAttr creation failed, retrying ...')
         time.sleep(1)
-        ok = create_bundle_attr(url, 'v2.kismis@nextensio.net', tenant, ['ABU,BBU'], ['engineering','sales'], 1, 1, "allowed")
+        ok = create_bundle_attr(url, bundle3attrjson)
 
-    ok = create_route(url, tenant, 'test1@nextensio.net', 'kismis.org', 'v1')
+    route1json = {"tenant":tenant, "route":"test1@nextensio.net:kismis.org", "tag":"v1"}
+    route2json = {"tenant":tenant, "route":"test2@nextensio.net:kismis.org", "tag":"v2"}
+    
+    ok = create_route(url, route1json)
     while not ok:
         print('Route creation failed, retrying ...')
         time.sleep(1)
-        ok = create_route(url, tenant, 'test1@nextensio.net', 'kismis.org', 'v1')
+        ok = create_route(url, route1json)
         
-    ok = create_route(url, tenant, 'test2@nextensio.net', 'kismis.org', 'v2')
+    ok = create_route(url, route2json)
     while not ok:
         print('Route creation failed, retrying ...')
         time.sleep(1)
-        ok = create_route(url, tenant, 'test2@nextensio.net', 'kismis.org', 'v2')
+        ok = create_route(url, route2json)
 
     with open('policy.AccessPolicy','r') as file:
         rego = file.read()
     ok = create_policy(url, tenant, 'AccessPolicy', rego)
     while not ok:
-        print('Policy creation failed, retrying ...')
+        print('Access Policy creation failed, retrying ...')
         time.sleep(1)
         ok = create_policy(url, tenant, 'AccessPolicy', rego)
 

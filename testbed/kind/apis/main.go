f3e8bc19268ae970e4f37da8e91aad7cf8bb99f0
Gopa Kumar <gopakumarce@gmail.com>
2022-03-19 10:54:50 -0400

    basic apis

--- testbed/kind/apis/main.go
+++ testbed/kind/apis/main.go
@@ -6,29 +6,47 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	apis "gitlab.com/nextensio/apis/controller/go"
 )
+
+var ctx context.Context
+var client *apis.APIClient
+
+func is_controller_up() bool {
+	_, resp, err := client.DefaultApi.GetTenants(ctx, "superadmin")
+	if err != nil || resp == nil {
+		fmt.Println("Error calling APIs", err)
+		return false
+	}
+	if resp.StatusCode != 200 {
+		fmt.Println("Bad response code", resp.StatusCode)
+		return false
+	}
+	return true
+}
 
 func main() {
 	tokens := authenticate("https://dev-635657.okta.com")
 	if tokens == nil {
 		os.Exit(1)
 	}
-	fmt.Println(tokens.AccessToken)
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	ctx := context.WithValue(context.Background(), apis.ContextAccessToken, tokens.AccessToken)
-	// create the API client
-	client := apis.NewAPIClient(apis.NewConfiguration())
-	tenants, resp, err := client.DefaultApi.GetTenants(ctx, "superadmin")
-	if err != nil || resp == nil {
-		fmt.Println("Error calling APIs", err)
-		return
+	ctx = context.WithValue(context.Background(), apis.ContextAccessToken, tokens.AccessToken)
+	client = apis.NewAPIClient(apis.NewConfiguration())
+
+	for !is_controller_up() {
+		fmt.Println("Controller not up, waiting ...")
+		time.Sleep(5 * time.Second)
 	}
-	if resp.StatusCode != 200 {
-		fmt.Println("Bad response code", resp.StatusCode)
-		return
+
+	ok, resp, err := client.DefaultApi.AddClientid(ctx, apis.AddClientId{Clientid: "0oaz5lndczD0DSUeh4x6"}, "superadmin")
+	for err != nil || (resp != nil && resp.StatusCode != 200) || ok.Result != "ok" {
+		fmt.Println("Clientid creation failed, retrying ...")
+		ok, resp, err = client.DefaultApi.AddClientid(ctx, apis.AddClientId{Clientid: "0oaz5lndczD0DSUeh4x6"}, "superadmin")
 	}
-	fmt.Println(tenants)
+
+	fmt.Println("APIs completed succesfully")
 }

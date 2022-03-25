48785c70a63edb020d409c5656e9d03eb06c94a1
Gopa Kumar <gopakumarce@gmail.com>
2022-03-25 10:08:15 -0400

    automate CA generation

--- testCert/cert.sh
+++ testCert/cert.sh
@@ -3,10 +3,9 @@
 # Become a Certificate Authority
 
 # Generate private key
-openssl genrsa -des3 -out nextensio.key 2048
-# Generate root certificate - all passwords should be Nextensio123 because that is used in automation/testbed/kind 
-# to generate controller/gateway certs
-openssl req -x509 -new -nodes -key nextensio.key -sha256 -days 825 -out nextensio.crt
+openssl genrsa -des3 -passout pass:"Nextensio123" -out nextensio.key 2048
+# Generate root certificate
+openssl req -x509 -new -passin pass:"Nextensio123" -config openssl.conf -nodes -key nextensio.key -sha256 -days 825 -out nextensio.crt
 
 # Now in chrome, under Privacy and Security, go to 'security'-->'Manage certificates' and
 # selet the 'Authorities' tab and click "Import" and import the nextensio.crt file
